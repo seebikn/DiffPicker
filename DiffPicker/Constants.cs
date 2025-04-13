e474bf1926d6c2c5eabef5c493fd0d0033cfb260
seebikn <stserver.bise@gmail.com>
2025-04-13 15:28:14 +0900

    不足ファイルの追加

---
+++ DiffPicker/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace DiffPicker.Controllers
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// メインウィンドウ
+        /// </summary>
+        public static class IniMain
+        {
+            public const string section = "Main";
+
+            public const string diffFolderName = "DiffFolderName";
+            public const string omitFile = "OmitFile";
+            public const string omitFolder = "OmitFolder";
+        }
+    }
+}
